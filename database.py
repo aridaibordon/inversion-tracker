a2514387a5c2d4b08be1b8321791a979cdf90d65
Ariday Bordon <aridaybordon@gmail.com>
2021-12-07 17:33:03 +0000

    Update database.py

--- database.py
+++ database.py
@@ -3,16 +3,33 @@ import sqlite3
 def create_database() -> None:
     con = sqlite3.connect("inversion.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(date()))")
-    cur.execute("CREATE TABLE crypto (id integer PRIMARY KEY, coin text, crp_balance float, eur_balance float, orderDate date DEFAULT(date()))")
-    cur.execute("CREATE TABLE operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(date()))")
-
+    cur.execute("CREATE TABLE degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))")
+    cur.execute("CREATE TABLE operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)")
+    cur.execute("CREATE TABLE portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)")
 
 def update_degiro(balance: float) -> None:
     con     = sqlite3.connect("inversion.db")
     cur     = con.cursor()
+    
+    cur.execute("INSERT INTO degiro (balance) VALUES (?)", (balance,))
+    con.commit()
+    cur.close()
 
-    cur     = cur.execute("INSERT INTO degiro (balance) VALUES (?)", (balance,))
+def add_operation(active, number, cpu) -> None:
+    con     = sqlite3.connect("inversion.db")
+    cur     = con.cursor()
+
+    cur.execute("INSERT INTO operations (active, number, cpu) VALUES (?, ?, ?)", (active, number, cpu))
+    con.commit()
+    cur.close()
+
+def get_portfolio() -> None:
+    con     = sqlite3.connect("inversion.db")
+    cur     = con.cursor()
+
+    cur.execute("SELECT active, SUM(number), crypto FROM operations GROUP BY active")
+    for active, number, crypto in cur.fetchall():
+        cur.execute("INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)", (active, number, crypto))
     
     con.commit()
     cur.close()
