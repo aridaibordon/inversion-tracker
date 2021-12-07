a9c64dd7db2555b48a66983454d957044e854034
Ariday Bordon <aridaybordon@gmail.com>
2021-12-07 10:55:40 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,3 @@ from env.config import TOKEN, CHAT_ID
 def send_degiro(balance):
     bot = Bot(token=TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=f"Tu portfolio total en DEGIRO asciende a {balance}€")
-
-if __name__=="__main__":
-    pass
