f424c98057812ffb73d45f6db680b3bae2534bf4
Ariday Bordon <aridaybordon@gmail.com>
2021-12-16 18:39:43 +0000

    v1.02

--- degiro.py
+++ degiro.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 USERNAME, PASSWORD = os.environ["USERNAME"], os.environ["PASSWORD"]
 
 options             = Options()
@@ -19,7 +21,7 @@ def text_to_float(text) -> float:
 def get_degiro_balance() -> float:
     # Returns balance of DEGIRO account
     service = Service("chromedriver.exe")
-    browser = webdriver.Chrome(service=service, options=options)
+    browser = webdriver.Chrome(service=ChromeDriverManager().install(), options=options)
 
     browser.get("https://www.degiro.es/login/")
 
