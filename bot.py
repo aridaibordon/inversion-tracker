b045803c605995431f36428f13d78a06b9438307
Ariday Bordon <aridaybordon@gmail.com>
2021-12-27 16:39:31 +0000

    new version

--- bot.py
+++ bot.py
@@ -20,9 +20,3 @@ def send_degiro():
     
     bot = Bot(token=TOKEN)
     bot.send_message(chat_id=CHAT_ID, text=f"Tu portfolio total en DEGIRO es {bal} ({dif_to_str(dif)})€")
-
-con         = sqlite3.connect("inversion.db")
-cur         = con.cursor()
-sel         = cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT 2").fetchall()
-
-print(sel)
