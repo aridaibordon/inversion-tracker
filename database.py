4e87c77b45900da632235df2709ec853a33301e8
Ariday Bordon <aridaybordon@gmail.com>
2022-01-12 09:17:59 +0000

    Update database.py

--- database.py
+++ database.py
@@ -4,15 +4,9 @@ import sqlite3
 def create_database() -> None:
     con = sqlite3.connect("inversion.db")
     cur = con.cursor()
-    cur.execute(
-        "CREATE TABLE [IF NOT EXISTS] degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))"
-    )
-    cur.execute(
-        "CREATE TABLE [IF NOT EXISTS] operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)"
-    )
-    cur.execute(
-        "CREATE TABLE [IF NOT EXISTS] portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)"
-    )
+    cur.execute("CREATE TABLE degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))")
+    cur.execute("CREATE TABLE operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)")
+    cur.execute("CREATE TABLE portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)")
 
 
 def update_degiro(balance: float) -> None:
@@ -28,10 +22,7 @@ def add_operation(active, number, cpu, crypto) -> None:
     con = sqlite3.connect("inversion.db")
     cur = con.cursor()
 
-    cur.execute(
-        "INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)",
-        (active, number, cpu, crypto),
-    )
+    cur.execute("INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)", (active, number, cpu, crypto))
     con.commit()
     cur.close()
 
@@ -42,10 +33,7 @@ def get_portfolio() -> None:
 
     cur.execute("SELECT active, SUM(number), crypto FROM operations GROUP BY active")
     for active, number, crypto in cur.fetchall():
-        cur.execute(
-            "INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)",
-            (active, number, crypto),
-        )
+        cur.execute("INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)", (active, number, crypto))
 
     con.commit()
     cur.close()
