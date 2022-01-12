eb0b487d7d51fc28139d3facdd4d1728d93b19f1
Ariday Bordon <aridaybordon@gmail.com>
2022-01-12 08:09:10 +0000

    change format

--- database.py
+++ database.py
@@ -1,35 +1,51 @@
 import sqlite3
 
+
 def create_database() -> None:
     con = sqlite3.connect("inversion.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))")
-    cur.execute("CREATE TABLE operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)")
-    cur.execute("CREATE TABLE portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)")
+    cur.execute(
+        "CREATE TABLE degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))"
+    )
+    cur.execute(
+        "CREATE TABLE operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)"
+    )
+    cur.execute(
+        "CREATE TABLE portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)"
+    )
+
 
 def update_degiro(balance: float) -> None:
-    con     = sqlite3.connect("inversion.db")
-    cur     = con.cursor()
-    
+    con = sqlite3.connect("inversion.db")
+    cur = con.cursor()
+
     cur.execute("INSERT INTO degiro (balance) VALUES (?)", (balance,))
     con.commit()
     cur.close()
 
-def add_operation(active, number, cpu, crypto) -> None:
-    con     = sqlite3.connect("inversion.db")
-    cur     = con.cursor()
 
-    cur.execute("INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)", (active, number, cpu, crypto))
+def add_operation(active, number, cpu, crypto) -> None:
+    con = sqlite3.connect("inversion.db")
+    cur = con.cursor()
+
+    cur.execute(
+        "INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)",
+        (active, number, cpu, crypto),
+    )
     con.commit()
     cur.close()
 
+
 def get_portfolio() -> None:
-    con     = sqlite3.connect("inversion.db")
-    cur     = con.cursor()
+    con = sqlite3.connect("inversion.db")
+    cur = con.cursor()
 
     cur.execute("SELECT active, SUM(number), crypto FROM operations GROUP BY active")
     for active, number, crypto in cur.fetchall():
-        cur.execute("INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)", (active, number, crypto))
-    
+        cur.execute(
+            "INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)",
+            (active, number, crypto),
+        )
+
     con.commit()
     cur.close()
