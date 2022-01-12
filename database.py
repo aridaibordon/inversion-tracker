d65ead35dd349ae5569d9cb24d4eec48f1dfd5a6
Ariday Bordon <aridaybordon@gmail.com>
2022-01-12 21:37:02 +0000

    rewrite connect method

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
 import sqlite3
 
 
-def create_database() -> None:
+def connect_dataset():
     con = sqlite3.connect("inversion.db")
-    cur = con.cursor()
+    return con, con.cursor()
+
+
+def return_balance() -> list:
+    con, cur = connect_dataset()
+    return cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT 2").fetchall()
+    
+
+def create_database() -> None:
+    con, cur = connect_dataset()
+
     cur.execute("CREATE TABLE IF NOT EXISTS degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))")
     cur.execute("CREATE TABLE IF NOT EXISTS operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)")
     cur.execute("CREATE TABLE IF NOT EXISTS portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)")
@@ -11,8 +21,7 @@ def create_database() -> None:
 
 
 def update_degiro_db(balance: float) -> None:
-    con = sqlite3.connect("inversion.db")
-    cur = con.cursor()
+    con, cur = connect_dataset()
 
     cur.execute("INSERT INTO degiro (balance) VALUES (?)", (balance,))
     con.commit()
@@ -20,8 +29,7 @@ def update_degiro_db(balance: float) -> None:
 
 
 def add_operation(active, number, cpu, crypto) -> None:
-    con = sqlite3.connect("inversion.db")
-    cur = con.cursor()
+    con, cur = connect_dataset()
 
     cur.execute("INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)", (active, number, cpu, crypto))
     con.commit()
@@ -29,8 +37,7 @@ def add_operation(active, number, cpu, crypto) -> None:
 
 
 def get_portfolio() -> None:
-    con = sqlite3.connect("inversion.db")
-    cur = con.cursor()
+    con, cur = connect_dataset()
 
     cur.execute("SELECT active, SUM(number), crypto FROM operations GROUP BY active")
     for active, number, crypto in cur.fetchall():
