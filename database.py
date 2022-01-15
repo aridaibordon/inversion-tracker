febcc2cf70d4fb6abacd4c3d88f683396d71b00a
Ariday Bordon <aridaybordon@gmail.com>
2022-01-15 16:19:06 +0000

    change sqlite to posgresql

--- database.py
+++ database.py
@@ -1,23 +1,37 @@
-import sqlite3
+import os
+import psycopg2
 
 
 def connect_dataset():
-    con = sqlite3.connect("inversion.db")
+    con = psycopg2.connect(
+        host=os.environ["SERVER"],
+        dbname=os.environ["DATABASE"],
+        user=os.environ["USERSQL"],
+        password=os.environ["PASSSQL"]
+        )
     return con, con.cursor()
 
 
+def close_session(connection, cursor):
+    cursor.close()
+    connection.close()
+
+
 def return_balance() -> list:
-    con, cur = connect_dataset()
-    return cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT 2").fetchall()
+    con, cur    = connect_dataset()
+    balance     = cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT 2").fetchall()
+    close_session(con, cur)
+    return balance
     
 
 def create_database() -> None:
     con, cur = connect_dataset()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS degiro (id integer PRIMARY KEY, balance float, orderDate date DEFAULT(datetime()))")
-    cur.execute("CREATE TABLE IF NOT EXISTS operations (id integer PRIMARY KEY, active text, number float, cost float, orderDate date DEFAULT(datetime()), crypto bool)")
-    cur.execute("CREATE TABLE IF NOT EXISTS portfolio (id integer PRIMARY KEY, active text, number float, orderDate date DEFAULT(datetime()), crypto bool)")
+    cur.execute("CREATE TABLE degiro (id integer BIGSERIAL NOT NULL PRIMARY KEY, balance float NOT NULL, orderDate date NOT NULL DEFAULT CURRENT_DATE)")
+    cur.execute("CREATE TABLE operations (id integer BIGSERIAL NOT NULL PRIMARY KEY, active text NOT NULL, number float NOT NULL, cost float NOT NULL, orderDate date NOT NULL DEFAULT CURRENT_DATE, crypto bool NOT NULL)")
+    cur.execute("CREATE TABLE portfolio (id integer BIGSERIAL NOT NULL PRIMARY KEY, active text NOT NULL, number float NOT NULL, orderDate date NOT NULL DEFAULT CURRENT_DATE, crypto bool NOT NULL)")
     con.commit()
+    close_session(con, cur)
 
 
 def update_degiro_db(balance: float) -> None:
@@ -25,7 +39,7 @@ def update_degiro_db(balance: float) -> None:
 
     cur.execute("INSERT INTO degiro (balance) VALUES (?)", (balance,))
     con.commit()
-    cur.close()
+    close_session(con, cur)
 
 
 def add_operation(active, number, cpu, crypto) -> None:
@@ -33,7 +47,7 @@ def add_operation(active, number, cpu, crypto) -> None:
 
     cur.execute("INSERT INTO operations (active, number, cpu, crypto) VALUES (?, ?, ?, ?)", (active, number, cpu, crypto))
     con.commit()
-    cur.close()
+    close_session(con, cur)
 
 
 def get_portfolio() -> None:
@@ -44,4 +58,4 @@ def get_portfolio() -> None:
         cur.execute("INSERT INTO portfolio (active, number, crypto) VALUES (?, ?, ?)", (active, number, crypto))
 
     con.commit()
-    cur.close()
+    close_session(con, cur)
