2660231c5aa29a158867c90e0116eadf6c0cf581
Ariday Bordon <aridaybordon@gmail.com>
2022-01-17 11:45:59 +0000

    Update bot.py

--- scripts/bot.py
+++ scripts/bot.py
@@ -6,16 +6,11 @@ from scripts.database import return_balance
 TOKEN, CHAT_ID = os.environ["TOKEN"], os.environ["CHAT_ID"]
 
 
-def dif_to_str(dif):
-    if dif > 0:
-        return "+" + str(dif)
-    return str(dif)
-
-
 def send_degiro():
     bot = Bot(token=TOKEN)
+    
     data = return_balance(2)
-    bot.send_message(chat_id=CHAT_ID, text=f"Tu portfolio total en DEGIRO es {data[0]}" + f" ({dif_to_str(data[0]-data[1])})"*(len(data)==2) + "€")
+    bot.send_message(chat_id=CHAT_ID, text=f"Tu portfolio total en DEGIRO es {data[0]}" + f" ({(data[0]-data[1])/data[1]:+.2%})"*(len(data)==2) + "€")
 
 
 def send_weekly_report():
