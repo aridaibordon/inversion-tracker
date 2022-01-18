14ee82865aeac5b9e24e35a06b1a397ab7083e66
Ariday Bordon <aridaybordon@gmail.com>
2022-01-18 21:50:24 +0000

    Add weekly reports

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,6 +1,7 @@
 import os
 
-from telegram import Bot
+from datetime import date
+from telegram import ParseMode, Bot
 from scripts.database import return_balance
 
 TOKEN, CHAT_ID = os.environ["TOKEN"], os.environ["CHAT_ID"]
@@ -8,12 +9,22 @@ TOKEN, CHAT_ID = os.environ["TOKEN"], os.environ["CHAT_ID"]
 
 def send_degiro():
     bot = Bot(token=TOKEN)
-    
-    data = return_balance(2)
-    bot.send_message(chat_id=CHAT_ID, text=f"Tu portfolio total en DEGIRO es {data[0]}" + f" ({(data[0]-data[1])/data[1]:+.2%})"*(len(data)==2) + "€")
+
+    today       = date.today().strftime('%d/%m/%Y')
+    now, last   = return_balance(2)
+    dif         = (now - last) / last
+
+    text   = f'<b>Daily report</b> ({today})\nYour account\'s balance is {now:.2f}€ ({dif:+.2%}).'
+    bot.send_message(chat_id=CHAT_ID, text=text, parse_mode=ParseMode.HTML)
 
 
 def send_weekly_report():
-    bot = Bot(token=TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text='Tu informe semanal está listo.')
+    bot     = Bot(token=TOKEN)
+
+    week    = date.today().strftime('%U/%Y')
+    balance = return_balance(5)
+    dif     = (balance[0] - balance[-1]) / balance[-1]
+
+    text    = f'<b>Weekly report</b> ({week})\nDuring this week, your investment have yield a total of {dif:+.2%}.'
+    bot.send_message(chat_id=CHAT_ID, text=text, parse_mode=ParseMode.HTML)
     bot.send_photo(chat_id=CHAT_ID, photo=open('plots/weekly-report.png', 'rb'))
