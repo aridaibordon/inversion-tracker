db4fcd2c16f278432bab9fd98d19c76775996ed6
Ariday Bordon <aridaybordon@gmail.com>
2022-02-02 14:21:57 +0000

    enhancement: New function to add addresses

--- scripts/crypto.py
+++ scripts/crypto.py
@@ -4,7 +4,19 @@ import json
 import yfinance as yf
 
 
-def get_address_balance(address: int) -> int:
+def load_addresses() -> list:
+    # Load all addresses in address.txt
+    with open('scripts/address.txt', 'r') as file:
+        return file.read().split()
+
+
+def add_new_address(address) -> None:
+    # Add new address to address.txt
+    with open('scripts/address.txt', 'a') as file:
+        file.write(f'{address}\n')
+
+
+def get_address_balance(address: int) -> float:
     # Return address balance in euros.
     r       = requests.get(f"https://blockchain.info/rawaddr/{address}")
     data    = json.loads(r.text)
@@ -14,3 +26,15 @@ def get_address_balance(address: int) -> int:
     if "final_balance" in data.keys():
         return data["final_balance"]*btc_value*1e-8
     raise ValueError("'address' is not valid or have not been found")
+
+
+def get_total_balance() -> float:
+    # Return balance of all addresses in address.txt
+    balance = 0
+    for address in load_addresses():
+        balance += get_address_balance(address)
+    return balance
+
+
+if __name__ == '__main__':
+    get_total_balance()
