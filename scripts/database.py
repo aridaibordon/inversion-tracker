175cbad8df84da39a382737769a96fcf87b2b3a1
Ariday Bordon <aridaybordon@gmail.com>
2022-02-09 10:57:54 +0000

    fix: remove unused functions

--- scripts/database.py
+++ scripts/database.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 
-def connect_dataset():
+def connect_database():
     con = psycopg2.connect(
         host        = os.environ["SERVER"],
         dbname      = os.environ["DATABASE"],
@@ -15,46 +15,38 @@ def connect_dataset():
 def close_session(connection, cursor):
     cursor.close()
     connection.close()
-
-
-def return_balance(number) -> list:
-    con, cur    = connect_dataset()
-    cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT %s", (str(number),))
-    balance = [item[0] for item in cur.fetchall()]
-    close_session(con, cur)
-    return balance
     
 
 def create_database() -> None:
-    con, cur = connect_dataset()
+    con, cur = connect_database()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS degiro (id bigserial NOT NULL PRIMARY KEY, balance float NOT NULL, orderDate date NOT NULL DEFAULT CURRENT_DATE)")
+    cur.execute('CREATE TABLE IF NOT EXISTS degiro (id bigserial NOT NULL PRIMARY KEY, balance float NOT NULL, orderDate date NOT NULL DEFAULT CURRENT_DATE)')
+    cur.execute('CREATE TABLE IF NOT EXISTS address (id bigserial NOT NULL PRIMARY KEY, address text NOT NULL')
     con.commit()
     close_session(con, cur)
 
 
 def update_degiro_db(balance: float) -> None:
-    con, cur = connect_dataset()
+    con, cur = connect_database()
 
     cur.execute("INSERT INTO degiro (balance) VALUES (%s)", (balance,))
     con.commit()
     close_session(con, cur)
 
 
-def add_operation(active, number, cpu, crypto) -> None:
-    con, cur = connect_dataset()
-
-    cur.execute("INSERT INTO operations (active, number, cost, crypto) VALUES (%s, %s, %s, %s)", (active, number, cpu, crypto))
-    con.commit()
+def get_addresses() -> tuple:
+    con, cur = connect_database()
+    
+    data = cur.execute('SELECT address FROM address')
     close_session(con, cur)
+    
+    return data.fetchall()
 
 
-def get_portfolio() -> None:
-    con, cur = connect_dataset()
-
-    cur.execute("SELECT active, SUM(number), crypto FROM operations GROUP BY active")
-    for active, number, crypto in cur.fetchall():
-        cur.execute("INSERT INTO portfolio (active, number, crypto) VALUES (%s, %s, %s)", (active, number, crypto))
-
-    con.commit()
+def return_balance(number) -> list:
+    con, cur    = connect_database()
+    cur.execute("SELECT balance FROM degiro ORDER BY id DESC LIMIT %s", (str(number),))
+    balance = [item[0] for item in cur.fetchall()]
     close_session(con, cur)
+    return balance
+
